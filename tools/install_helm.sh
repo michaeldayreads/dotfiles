1d7a5e55a27e158e42afdd699d9b4e16d3ad6bbd
Michael Day <michaeldayreads@gmail.com>
2018-03-07 22:59:23 +0000

    revise helm install, reinstall

--- tools/install_helm.sh
+++ tools/install_helm.sh
@@ -4,6 +4,10 @@ set -ex
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
 chmod 700 get_helm.sh
 ./get_helm.sh
+sleep 1s
+rm get_helm.sh
 kubectl create -f configs/k8s/helm/rbac-config.yaml
+sleep 5s
 helm init --service-account tiller
+sleep 10s # Give helm a little time to come up before asking for version
 helm version
--- tools/re_install_helm.sh
+++ tools/re_install_helm.sh
@@ -7,5 +7,5 @@ sleep 5s
 kubectl create -f configs/k8s/helm/rbac-config.yaml
 sleep 5s
 helm init --upgrade --service-account tiller
-sleep 5s
+sleep 10s # Give helm a little time to come up before asking for version
 helm version
